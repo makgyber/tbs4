8f0eacfbad479bbfc290daa185e16f82e1fda77d
Jon Mereria <makgyber@gmail.com>
2025-10-18 22:15:02 +0800

    add filters

--- app/Filament/Pages/RetentionBoard.php
+++ app/Filament/Pages/RetentionBoard.php
@@ -3,11 +3,19 @@
 namespace App\Filament\Pages;
 
 use App\Models\Retention;
+use Filament\Forms\Components\DatePicker;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Pages\Page;
+use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Components\Text;
 use Filament\Schemas\Schema;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\Width;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\Indicator;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 use Relaticle\Flowforge\Board;
 use Relaticle\Flowforge\BoardPage;
 use Relaticle\Flowforge\Column;
@@ -33,15 +41,55 @@ class RetentionBoard extends BoardPage
             ->cardSchema(fn(Schema $schema) => $schema        // Rich card content
             ->components([
                 TextEntry::make('summary')
-                    ->size('lg')
-                    ->hiddenLabel(),
+                    ->size('md')
+                    ->hiddenLabel()
+                    ->columnSpanFull(),
                 TextEntry::make('assignedTo.name')
                     ->color(Color::Emerald)
+                    ->size('xs')
                     ->badge()->hiddenLabel(),
-                TextEntry::make('created_at')->date()->hiddenLabel(),
-            ]))
-            ->filters([
+                TextEntry::make('created_at')
+                    ->size('xs')
+                    ->date()->hiddenLabel(),
 
+            ])->columns(2))
+            ->filters([
+                SelectFilter::make('assignedTo')
+                    ->searchable()
+                    ->relationship('assignedTo', 'name'),
+                Filter::make('overdue')
+                    ->schema([
+                        DatePicker::make('created_from')
+                            ->default(now()->firstOfMonth()),
+                        DatePicker::make('created_until')
+                            ->default(now()->lastOfMonth()),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    })
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+
+                        if ($data['created_from'] ?? null) {
+                            $indicators[] = Indicator::make('Created from ' . Carbon::parse($data['created_from'])->toFormattedDateString())
+                                ->removeField('created_from');
+                        }
+
+                        if ($data['created_until'] ?? null) {
+                            $indicators[] = Indicator::make('Created until ' . Carbon::parse($data['created_until'])->toFormattedDateString())
+                                ->removeField('created_until');
+                        }
+
+                        return $indicators;
+                    }),
             ]);
 
     }
